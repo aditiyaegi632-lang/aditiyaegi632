6abdfecfee5773266bf5b01db5743b134cf6812c
aditiyaegi632-lang <aditiyaegi632@gmail.com>
2025-11-22 19:37:42 +0700

    m

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ function animateProgressBars(){
    - Highlight nav link aktif berdasarkan section yang terlihat
    ---------------------------- */
 const sections = document.querySelectorAll(
-  '#about-detail, #education, #experience, #skills, #contact'
+  '#about, #education, #experience, #skills, #contact'
 );
 const spyOptions = {
   root: null,
